b1568c55a1b992584a9f3ae5460d147abd3a6ac5
DorindaAbeyta <129131324+DorindaAbeyta@users.noreply.github.com>
2023-05-20 14:45:49 -0600

    completed post.ts, added post.controller.ts and post.route.ts

--- backend/src/utils/models/Post.ts
+++ backend/src/utils/models/Post.ts
@@ -17,9 +17,10 @@ export async function insertPost (post: Post): Promise<string> {
     return 'post created successfully'
 }
 
+
 // TODO ask instructors about the oder by in this function
-export async function selectAllRecentPosts (): Promise<Post[]> {
-     return <Post[]> await sql `SELECT post_id, post_profile_id, post_content, post_date_time, post_profile_handle_is_visible, post_title FROM post ORDER BY post_date_time DESC`
+export async function selectAllPosts (): Promise<Post[]> {
+     return <post[]> await sql `SELECT post_id, post_profile_id, post_content, post_date_time, post_profile_handle_is_visible, post_title FROM post`
 }
 // TODO ask how we would select all by vote count
 
@@ -31,3 +32,15 @@ export async function selectPostsByPostId (postId: string): Promise<Post|null> {
 export async function selectPostsByPostProfileId (postProfileId: string): Promise<Post[]> {
     return <Post[]> await sql`SELECT post_id, post_profile_id, post_content, post_date_time, post_profile_handle_is_visible, post_title FROM post WHERE post_profile_id = ${postProfileId}`
 }
+
+export async function deletePostbyPostId (post: Post): Promise<string> {
+    const {postId, postProfileId} = post
+    await sql`DELETE FROM "post" WHERE post_id = ${postId} AND post_profile_id ${postProfileId}`
+    return 'Post deleted successfully'
+}
+
+// TODO ask how we would write this and select from postIsPublished
+// export async function selectPostsByPostProfileHandleIsVisible (postProfileHandleIsVisible: boolean) Promise<Post[]> {
+//     return = <Post[]> await sql`SELECT post_id, post_profile_id, post_content, post_date_time, post_profile_handle_is_visible, post_title FROM post WHERE post_profile_handle_is_visible = ${selectPostsByPostProfileHandleIsVisible}`
+// }
+
