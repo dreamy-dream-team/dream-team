197860d8a0630cabec7ee3af96ff522f6d76a28a
Jasmine <jwilliams261@cnm.edu>
2023-05-25 16:55:59 -0600

    added content to files

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -24,7 +24,7 @@ export class App {
         private readonly port?: number | string
     ) {
 
-        this.redisClient = createClient({ legacyMode: true, socket: { host: process.env.REDIS_HOST } })
+        this.redisClient = createClient({ socket: { host: process.env.REDIS_HOST } })
         this.redisClient.connect().catch(console.error)
 
         this.redisStore = new RedisStore({client: this.redisClient})
@@ -63,7 +63,7 @@ export class App {
         this.app.use('/apis/vote', VoteRoute)
         this.app.use('/apis/post', PostRoute)
         this.app.use('/apis/category', CategoryRoute)
-        this.app.use('apis/post-category', PostCategoryRoute)
+        this.app.use('/apis/post-category', PostCategoryRoute)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
--- backend/src/utils/models/Post-Category.ts
+++ backend/src/utils/models/Post-Category.ts
@@ -7,7 +7,7 @@ export interface PostCategory {
 
 export async function insertPostCategory (postCategory: PostCategory): Promise<string> {
     const {postCategoryCategoryId, postCategoryPostId} = postCategory
-    await sql `INSERT INTO  post_category (post_category_category_id, post_category_post_id VALUES${postCategoryCategoryId}, ${postCategoryPostId}) `
+    await sql `INSERT INTO  post_category (post_category_category_id, post_category_post_id) VALUES (${postCategoryCategoryId}, ${postCategoryPostId})`
     return 'post category created successfully'
 }
 
@@ -19,8 +19,9 @@ export async function selectPostCategoryByPostId (postCategoryPostId: string): P
     return <PostCategory[]> await sql `SELECT post_category_category_id, post_category_post_id FROM post_category WHERE post_category_post_id = ${postCategoryPostId}`
 }
 
-export async function selectPostCategoryByPrimaryKey (postCategory: PostCategory): Promise<PostCategory[]> {
-    return <PostCategory[]> await sql `SELECT post_category_category_id, post_category_post_id FROM post_category WHERE post_category_category_id = ${postCategory.postCategoryCategoryId} AND post_category_post_id = ${postCategory.postCategoryPostId}`
+export async function selectPostCategoryByPrimaryKey (postCategoryCategoryId: string, postCategoryPostId: string): Promise<PostCategory|null> {
+    const result = <PostCategory[]> await sql `SELECT post_category_category_id, post_category_post_id FROM post_category WHERE post_category_category_id = ${postCategoryCategoryId} AND post_category_post_id = ${postCategoryPostId}`
+    return result.length === 1 ? result[0] : null
 }
 
 export async function deletePostCategory (postCategory: PostCategory): Promise<string> {
