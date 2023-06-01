5b449ee33249a973ed6c18bd8bf5f65c89d80f14
Jasmine <jwilliams261@cnm.edu>
2023-06-01 13:44:26 -0600

    started navbar creation

---
+++ frontend/src/ui/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+
+export const Navbar = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+
+        </Container>
+
+    )
+}
