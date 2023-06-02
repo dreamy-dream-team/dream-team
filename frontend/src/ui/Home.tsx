0f1328fcbc66046a1c0c5d7368eee0a762dac2d4
DorindaAbeyta <129131324+DorindaAbeyta@users.noreply.github.com>
2023-06-01 22:18:17 -0600

    Created homepage branch, started development on Home.tsx, added forms for title and dream-content, adjusted sizes.

--- frontend/src/ui/Home.tsx
+++ frontend/src/ui/Home.tsx
@@ -1,9 +1,26 @@
 import React from "react"
+import {Container, FloatingLabel, Form} from "react-bootstrap";
 
 export function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <section className={'text-center'}>
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Title"
+                    className="mb-1"
+                >
+                    <Form.Control type="title" placeholder="Title" style={{ height: '50px', width: '500px'}}/>
+                </FloatingLabel>
+
+                <FloatingLabel controlId="floatingTextarea2" label="Tell us your dream...">
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Tell us your dream..."
+                        style={{ height: '175px', width: '500px'}}
+                    />
+                </FloatingLabel>
+            </section>
         </>
-    )
+    );
 }
