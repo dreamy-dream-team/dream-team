cb58915cf24bf650afa57eca84c46f633c640741
Kimberly Olson <kolson21@cnm.edu>
2023-06-01 21:57:56 -0600

    started route

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import { Home } from './Home.tsx'
 import { FourOhFour } from './FourOhFour'
+import {CategoryMain} from "./CategoryMain/CategoryMain";
 
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
                 <Routes>
                     <Route  path='/' element={<Home />} />
                     <Route path={"*"} element={<FourOhFour />} />
+                    <Route path={'/category'} element={<CategoryMain />} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ frontend/src/ui/CategoryMain/CategoryMain.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {Col, Container, Row} from "react-bootstrap";
+
+export function CategoryMain() {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Category Main Page</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}``
