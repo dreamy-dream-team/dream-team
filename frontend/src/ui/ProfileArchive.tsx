7cbb42e9e5beb0afa6b8e7410c8e4acf898f4922
Mario Diaz <mdiaz20@cnm.edu>
2023-06-05 12:22:26 -0600

    Added three images to folder images Logo-(1,2, and 3) modified profilearchive

--- frontend/src/ui/ProfileArchive.tsx
+++ frontend/src/ui/ProfileArchive.tsx
@@ -1,5 +1,5 @@
 //import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 
 const categories = [
     "Funny",
@@ -22,6 +22,14 @@ export const ProfileArchive = () => {
     return (
         <>
             <Container fluid>
+                <Row className="justify-content-center text-center" style={{marginTop:'20px'}}>
+                    <Col xs ={12}>
+                        <Image src="/Images/Logo-1.jpeg" alt="Logo" />
+                    </Col>
+                    <Col xs={12}>
+                    <h1 className="mt-3">Dreamery Journal</h1>
+                    </Col>
+                </Row>
                 <Row style={{marginTop: '200px'}}>
                     <Col className="p-3" md={3} xs={12} style={{height: "100vh"}}>
                         <div className="border border-3 rounded p-2 bg-light h-100 d-flex flex-column justify-content-center align-items-center">
