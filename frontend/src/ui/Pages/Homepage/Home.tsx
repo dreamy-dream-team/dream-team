c9b675829cc3db0a40a7ea18a06230f3fa2f32b4
DorindaAbeyta <129131324+DorindaAbeyta@users.noreply.github.com>
2023-06-14 16:56:42 -0600

    final push

--- frontend/src/ui/Pages/Homepage/Home.tsx
+++ frontend/src/ui/Pages/Homepage/Home.tsx
@@ -1,10 +1,13 @@
 import {PostFormModal} from "../../PostForm/PostFormModal.tsx";
+import {SideNav} from "../../../shared/components/SideNav.tsx";
 
 
 export function Home() {
     return (
         <>
+            <SideNav />
             <PostFormModal/>
+
         </>
     )
 }
