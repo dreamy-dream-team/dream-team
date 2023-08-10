ad2898b4d4c0d440f9c6b9bc68852aa939015deb
kim <kolson22@gmail.com>
2023-08-09 21:46:54 -0600

    almost working pagination

--- frontend/src/ui/Pages/Homepage/Home.tsx
+++ frontend/src/ui/Pages/Homepage/Home.tsx
@@ -3,11 +3,20 @@ import {SideNav} from "../../../shared/components/SideNav.tsx";
 import {useGetAllPostsQuery} from "../../../store/apis.ts";
 import {PostCard} from "../../../shared/post-card/PostCard.tsx";
 import {Col, Container, Row} from "react-bootstrap";
+import Pagination from "../../../shared/components/pagination/Pagination.tsx";
+import {useState} from "react";
 
 
 export function Home() {
     const {data} = useGetAllPostsQuery("")
     const posts = data?? []
+
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage] = useState(10)
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
     return (
         <>
             <SideNav />
@@ -16,7 +25,13 @@ export function Home() {
                 <Row className={"py-2"}>
                     <Col>
                         {posts.map(post => <PostCard post={post} key={post.postId}/>)}
+                        <Pagination
+                            postsPerPage={postsPerPage}
+                            totalPosts={posts.length}
+                            paginate={paginate}
+                        />
                     </Col>
+
                 </Row>
             </Container>
         </>
